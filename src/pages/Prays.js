d861b4b9fe4860a9f8ab154b6dce571792522a6c
rakhimjonovjoxongir@gmail.com <jahongir.r1999@gmail.com>
2022-06-28 00:59:02 +0500

    prays full

--- src/pages/Prays.js
+++ src/pages/Prays.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import "../style/Prays.css"
+import React from "react";
+import Table from "../components/Table";
+import PraysData from "../data/PraysData";
+import "../style/Prays.css";
 
 const Prays = () => {
   return (
-    <div className='prays_container'>
-      assa
+    <div className="prays_container">
+      {PraysData.map((item) => {
+        return (
+          <div className="content">
+            <h2
+              key={item.id}
+              // ref={refs[item.id]}
+            >
+              {item.category}
+            </h2>
+            <table className="list">
+              {item.product.map((product, index) => {
+                return <Table key={index} {...product} />;
+              })}
+            </table>
+          </div>
+        );
+      })}
     </div>
   );
 };
